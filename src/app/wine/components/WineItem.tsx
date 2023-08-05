2e36b387e467e9e5cd55ff5f4cc8e8d14c7ffb55
HC-Gwon <gurckddlz@naver.com>
2023-08-06 02:27:52 +0900

    feat: 각 와인 아이템 컴포넌트 추가

---
+++ src/app/wine/components/WineItem.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { Button } from 'antd';
+
+import { Product } from '../models/WineType';
+import { deleteWine } from '../api/deleteWine';
+
+function WineItem({
+  wine,
+  refetchWineList,
+}: {
+  wine: Product;
+  refetchWineList: () => void;
+}) {
+  const router = useRouter();
+
+  const onClickRevise = (wineId: number) => {
+    router.push(`/revise-wine/${wineId}`);
+  };
+
+  const onClickDelete = async (wineId: number) => {
+    await deleteWine(wineId).then(() => {
+      refetchWineList();
+    });
+  };
+
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        <Image
+          src={wine.image ?? '/images/empty_wine.png'}
+          alt={wine.name}
+          width={100}
+          height={100}
+        />
+        <div>
+          <div style={{ fontSize: '20px' }}>{wine.name}</div>
+          <div style={{ display: 'flex', marginTop: '10px', gap: '10px' }}>
+            <Button onClick={() => onClickRevise(wine.wine._id)}>
+              정보 수정
+            </Button>
+            <Button danger onClick={() => onClickDelete(wine.id)}>
+              삭제
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: '1px',
+          backgroundColor: 'black',
+        }}
+      />
+    </div>
+  );
+}
+
+export default WineItem;
